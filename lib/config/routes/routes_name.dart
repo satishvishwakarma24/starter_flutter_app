8461a7c357a8bb87fabe66001a88a59eca58e771
Satish Vishwakarma <satish.vishwakarma.it@gmail.com>
2026-05-18 13:26:42 +0530

    only contain routesname - home & setting screen

--- lib/config/routes/routes_name.dart
+++ lib/config/routes/routes_name.dart
@@ -1,8 +1,5 @@
 // 🌐 ROUTE NAMES
 abstract class RoutesName {
   static const String homeScreen = '/';
-  static const String coinTosserScreen = '/coin-tosser';
-  static const String diceRollerScreen = '/dice-roller';
-  static const String rockPaperScissorsScreen = '/rock-paper-scissors';
-  static const String yesNoMaybeScreen = '/yes-no-maybe';
+  static const String settingsScreen = '/settings';
 }
